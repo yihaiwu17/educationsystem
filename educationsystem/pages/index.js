da2dd79b82ac8a7dcff2d9a4012556165864e87f
yihaiwu17 <49830887+yihaiwu17@users.noreply.github.com>
2020-11-24 12:16:10 +1300

    enviroument built

---
+++ educationsystem/pages/index.js
@@ -0,0 +1,11 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import { Button } from "antd";
+
+export default function Home() {
+  return (
+    <div className="App">
+      <Button type="primary">Button</Button>
+    </div>
+  );
+}
